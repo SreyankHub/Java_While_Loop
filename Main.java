16f1b5714224b8c7a970535ea67009d28c05baaf
SreyankHub <96630625+SreyankHub@users.noreply.github.com>
2022-03-11 22:41:54 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+      String input = "";
+
+      while(input.isBlank()) {
+
+          System.out.print("What is Your name:");
+          input = scanner.nextLine();
+          System.out.print("You Have Escaped The Wile Loop:");
+          System.out.print(input);
+
+      }
+    }
+}
